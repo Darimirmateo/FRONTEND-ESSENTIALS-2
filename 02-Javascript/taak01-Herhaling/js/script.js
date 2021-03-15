34ccc9413974d68ad75d7884a2502fbc53920c7b
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-03-15 11:20:35 +0100

    working

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,14 @@
 let strNaam;
 let boolNaamKlopt;
+
+
+
+
+strNaam = prompt(" wat is jouw naam?");
+// alert("Ey!" + strNaam); 
+// console.log(strNaam + " is " + boolNaamKlopt + " years old");
+console.log(strNaam + boolNaamKlopt);
+boolNaamKlopt = confirm("Heet je echt" + strNaam +"?"); 
+
+alert("topper bedankt!");
+
---
+++ 02-Javascript/taak02-Functions-introductie/js/script.js
@@ -0,0 +1,25 @@
+//declaratie van de functie
+function toonAlertBox(){
+    alert("are you ok/?");
+}
+
+//aanroep van de functie
+toonAlertBox();
+
+
+
+
+
+// let strNaam;
+// let boolNaamKlopt;
+
+
+
+
+// strNaam = prompt(" wat is jouw naam?");
+// alert("Ey!" + strNaam); 
+// console.log(strNaam + " is " + boolNaamKlopt + " years old");
+// console.log(strNaam + boolNaamKlopt);
+// boolNaamKlopt = confirm("Heet je echt" + strNaam +"?"); 
+
+// alert("topper bedankt!");
