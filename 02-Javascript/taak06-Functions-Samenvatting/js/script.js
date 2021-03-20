9ca4b63d89c654767c9a9d749f1a8b800bd88a4b
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-03-20 15:29:53 +0100

    working

--- 02-Javascript/taak06-Functions-Samenvatting/js/script.js
+++ 02-Javascript/taak06-Functions-Samenvatting/js/script.js
@@ -1,14 +1,18 @@
 var score = 0;
 let getal1;
+l
 
 function makeRandomNumber(){
     return Math.floor(Math.random()*99+1);
+
 }
 
 function showRandomNumber1(){
     let randomGetal = makeRandomNumber()
-    document.querySelector(".getal1").innerHTML = randomGetal;
-    return randomGetal;
+    document.querySelector(".getal1" ).innerHTML = randomGetal;
+    return getal1;
 }
 
 getal1 = showRandomNumber1();
+score = getal1;
+
