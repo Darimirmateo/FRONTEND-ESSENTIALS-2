595b7fffc5beaa2c39747b73638e335769dc7b94
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-03-23 12:09:53 +0100

    working mode

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,35 @@
+film0 = "The Godfather";
+film1 = "The Shawshank Redemption";
+film2 = "Schindler's List";
+film3 = "Raging Bull";
+film4 = "Casablanca";
+film5 = "Citizen Kane";
+film6 = "Gone with the Wind";
+film7 = "The Wizard of Oz";
+film8 = "One Flew Over the Cuckoo's Nest";
+film9 = "Lawrence of Arabia";
+
+
+
+topGenres = ["LatinMusic", "Edm", "Rap","HipHop", "Salsa", "BaileFunk", "House","Trap", "Rnb", "Reaggeton"]; //definitie van array
+console.log(topGenres[0]); 
+console.log(topGenres[1]); 
+console.log(topGenres[2]); 
+console.log(topGenres[3]); 
+console.log(topGenres[4]);
+console.log(topGenres[5]);
+console.log(topGenres[6]);
+
+
+
+topArtiesten = ["Drake", "Eminem", "Michale Jackson","Elvis presley", "Stivie Wonder", "Rihanna", "Lil wayne","Beyonce", "Tempo", "Daddy yankee"]; //definitie van array
+console.log(topArtiesten[0]); 
+console.log(topArtiesten[1]); 
+console.log(topArtiesten[2]); 
+console.log(topArtiesten[3]); 
+console.log(topArtiesten[4]);
+console.log(topArtiesten[5]);
+console.log(topArtiesten[6]);
+
+
+
