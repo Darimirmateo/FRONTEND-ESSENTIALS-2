3d5c6d1507d4fe5303e7f4110146e92574edc7d0
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-04-13 12:07:44 +0200

    workingg geluktt

--- 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
+++ 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
@@ -11,6 +11,9 @@ topFilms = [
     "Lawrence of Arabia"
 ];
 
+
+
+
 eigenTopFilms = [];
 
 //met deze for-loop word de ordered list gevuld met film namen
@@ -18,9 +21,36 @@ for(i = 0; i < topFilms.length; i++){
     document.querySelector(".imdb-film" + (i+1)).innerHTML = topFilms[i];
 }
 
+
+
 function kiesFilm(){
+    var antw = prompt("Wat zijn je top 3 films ?");
+    console.log(antw);
+    var gekozenfilm = topFilms[antw-1];   
+    console.log(gekozenfilm);
+    eigenTopFilms.push(gekozenfilm);
+    document.querySelector(".eigen-film1").innerHTML=[1];
+
+    var antw = prompt("Wat zijn je top 3 films ?");
+    console.log(antw);
+    var gekozenfilm = topFilms[antw-1];   
+    console.log(gekozenfilm);
+    eigenTopFilms.push(gekozenfilm);
+    document.querySelector(".eigen-film2").innerHTML=[2];
+
+    var antw = prompt("Wat zijn je top 3 films ?");
+    console.log(antw);
+    var gekozenfilm = topFilms[antw-1];   
+    console.log(gekozenfilm);
+    eigenTopFilms.push(gekozenfilm);
+    document.querySelector(".eigen-film3").innerHTML=[3];
+
+
 
 }
+
+
+
 
 topFilms = ["The Godfather", "The Shawshank Redemption", "Schindler's List"];
 topFilmsOmgedraaid = []; //lege array, hierin komen de films uit topFilms in omgekeerde volgorden
@@ -30,3 +60,14 @@ topFilmsOmgedraaid.push(topFilms[1]);
 topFilmsOmgedraaid.push(topFilms[0]);
 
 console.log(topFilmsOmgedraaid);
+
+var antw = [0];
+
+
+
+// document.querySelector(topFilmsOmgedraaid).innerHTML = topFilms[0];
+
+
+// document.querySelector.(aantwoord.innerHTML = topFilms;
+
+
