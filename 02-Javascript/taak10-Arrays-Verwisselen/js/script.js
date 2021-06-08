117691e31e99897c5d8d94166d8d8b907279ba90
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-06-08 11:37:32 +0200

    Finished

--- 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
+++ 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
@@ -1,19 +1,16 @@
 topFilms = [
-    "The Godfather",
-    "The Shawshank Redemption",
-    "Schindler's List",
-    "Raging Bull",
-    "Casablanca"
-];
-
-
-topFilms = [
     "The Shawshank Redemption", // 0
     "The Godfather", // 1
     "Schindler's List", //2
     "Raging Bull",  //3
     "Casablanca"  //4
 ];
+
+
+
+var x = 0;
+
+
 
 for(var i=0;i < topFilms.length; i++){
     document.querySelector(".film-ranking"+(i+1)).innerHTML = i + 1;
@@ -23,39 +20,24 @@ for(var i=0;i < topFilms.length; i++){
 
 
 function moveup(film){
-    // console.log(film);
-    // document.getElementsByClassName("film-naam")[i].innerHTML = topFilms[i];
-    //  document.querySelector(".film-naam"+(film+1)).innerHTML = topFilms[i=0]
-        document.querySelector(".film-naam"+(film)).innerHTML = topFilms[film];
-        document.querySelector(".film-naam"+(film+1)).innerHTML = topFilms[film-1];
+    x = document.querySelector(".film-naam"+(film)).innerHTML;
+    document.querySelector(".film-naam"+(film)).innerHTML = document.querySelector(".film-naam"+(film+1)).innerHTML;
+    document.querySelector(".film-naam"+(film+1)).innerHTML = x;
         
-    
-     
-    
-    
-
 }
 
 function movedown(film){
-    // console.log(film);
-    // document.querySelector(".film-naam"+(film+1)).innerHTML = topFilms[0];
-    document.querySelector(".film-naam"+(film+2)).innerHTML = topFilms[film+1];
-    document.querySelector(".film-naam"+(film+1)).innerHTML = topFilms[film+1];
+    x = document.querySelector(".film-naam"+(film+1)).innerHTML;
+    document.querySelector(".film-naam"+(film+1)).innerHTML = document.querySelector(".film-naam"+(film+2)).innerHTML;
+    document.querySelector(".film-naam"+(film+2)).innerHTML = x;
 
 }
     
 
-
-
-function reset(topFilms){
-  
-// location.reload();
-
-document.getElementById("reset2").reset();
-
-
-
-
+function reset2(){
+    for(var i=0;i < topFilms.length; i++){
+      document.querySelector(".film-naam"+(i+1)).innerHTML = topFilms[i];
+    }
 }
 
  
