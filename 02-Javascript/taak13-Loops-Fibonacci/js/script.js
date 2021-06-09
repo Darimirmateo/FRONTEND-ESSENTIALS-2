e9ab86e9dc08a75e11441e47253a301d07121568
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-06-09 14:52:45 +0200

    yes

--- 02-Javascript/taak13-Loops-Fibonacci/js/script.js
+++ 02-Javascript/taak13-Loops-Fibonacci/js/script.js
@@ -1,9 +1,12 @@
-var fibonacci = [0,];
-
+// var fibonacci = [0,1];
+var fibonacci = [0,1,1,2];
 
 for (i = 0; i < 100; i++) {
-    if(i % 2!==0){
-        console.log(i);
+    if(i % 4!==0){
+        
+        fibonacci = fibonacci;
+        // fibonacci[i] = fibonacci[i - 5] + fibonacci[i - 3];
+        console.log(i); 
     }
     
 }
