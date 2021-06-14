a37c5748c30dc26c16e975cbbd9ba46df404298c
Darimir Mateo <darimir.mateo@student.rocva.nl>
2021-06-14 10:42:04 +0200

    working

--- 02-Javascript/taak14-Loops-Strings/js/script.js
+++ 02-Javascript/taak14-Loops-Strings/js/script.js
@@ -1,19 +1,11 @@
-function outputLetters(str){
-    for (let i = 0; i < str.length; i++) {
-        const element = array[i];
-        
-    }
-}
-
-var voorbeeldString = "Dit is een array van letters";
-console.log(voorbeeldString.length); // geeft terug:
 
 
 
-// var person = prompt("Yooo wat is je naam?");
 
-if (voorbeeldString != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + voorbeeldString + "bedankt topper! ";
-  console.log
+var voorbeeldArray = [14];
+
+for (let i = 0; i < voorbeeldArray.length; i++) {
+    voorbeeldArray[i] += 1;
+    prompt("wat is je naam?");
+    console.log(voorbeeldArray[i]);
 }
